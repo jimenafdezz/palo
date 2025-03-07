e83c64a6064b837799875686af4842b57ce3782d
jimenafdezz <jimena.fernandezlabrador@iesmiguelherrero.com>
2025-03-07 16:50:55 +0100

    Iñigo el de Ampuero

---
+++ src/main/java/mavenproyect/pallito/Pallito.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mavenproyect.pallito;
+
+/**
+ *
+ * @author jimen
+ */
+public class Pallito {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("noseee");
+    }
+    
+}

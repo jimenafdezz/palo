3e3e92ea580bb7bf1fae3c5b12af1286c0137357
jimenafdezz <153528628+jimenafdezz@users.noreply.github.com>
2025-03-07 16:54:04 +0100

    Update Pallito.java

--- src/main/java/mavenproyect/pallito/Pallito.java
+++ src/main/java/mavenproyect/pallito/Pallito.java
@@ -13,6 +13,7 @@ public class Pallito {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("noseee");
+        System.out.println("me gusta comer");
     }
     
 }
